2b666e8a1a8dd9e46e8104f89eaa61008ae95d72
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-12-22 15:18:54 +0000

    Initialised s2

--- MonitorSvcG2/grails-app/conf/Config.groovy
+++ MonitorSvcG2/grails-app/conf/Config.groovy
@@ -115,3 +115,20 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'ac.uk.jisc.monitor.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'ac.uk.jisc.monitor.UserRole'
+grails.plugin.springsecurity.authority.className = 'ac.uk.jisc.monitor.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                ['permitAll'],
+	'/index':           ['permitAll'],
+	'/index.gsp':       ['permitAll'],
+	'/assets/**':       ['permitAll'],
+	'/**/js/**':        ['permitAll'],
+	'/**/css/**':       ['permitAll'],
+	'/**/images/**':    ['permitAll'],
+	'/**/favicon.ico':  ['permitAll']
+]
+
