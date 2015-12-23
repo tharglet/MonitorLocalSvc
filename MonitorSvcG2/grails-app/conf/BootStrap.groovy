dade463aa3f8a3f2e99aab75973ffd2534836fec
Ian Ibbotson <ian.ibbotson@gmail.com>
2015-12-23 18:09:19 +0000

    Working on default accounts

--- MonitorSvcG2/grails-app/conf/BootStrap.groovy
+++ MonitorSvcG2/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,57 @@
+import ac.uk.jisc.monitor.*
+
 class BootStrap {
 
-    def init = { servletContext ->
+  def init = { servletContext ->
+
+    def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER', roleType:'global').save(failOnError: true)
+    def editorRole = Role.findByAuthority('ROLE_EDITOR') ?: new Role(authority: 'ROLE_EDITOR', roleType:'global').save(failOnError: true)
+    def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN', roleType:'global').save(failOnError: true)
+    def apiRole = Role.findByAuthority('ROLE_API') ?: new Role(authority: 'ROLE_API', roleType:'global').save(failOnError: true)
+
+    makeDefaultAccounts()
+  }
+
+
+  def destroy = {
+  }
+
+  def makeDefaultAccounts() {
+
+    grailsApplication.config.sysusers.each { su ->
+      log.debug("test ${su.name} ${su.pass} ${su.display} ${su.roles}");
+      def user = User.findByUsername(su.name)
+      if ( user ) {
+        if ( user.password != su.pass ) {
+          log.debug("Hard change of user password from config ${user.password} -> ${su.pass}");
+          user.password = su.pass;
+          user.save(failOnError: true)
+        }
+        else {
+          log.debug("${su.name} present and correct");
+        }
+      }
+      else {
+        log.debug("Create user...");
+        user = new User(
+                    username: su.name,
+                    password: su.pass,
+                    // display: su.display,
+                    // email: su.email,
+                    enabled: true).save(failOnError: true)
+      }
+
+      log.debug("Add roles for ${su.name}");
+      su.roles.each { r ->
+        def role = Role.findByAuthority(r)
+        if ( ! ( user.authorities.contains(role) ) ) {
+          log.debug("  -> adding role ${role}");
+          UserRole.create user, role
+        }
+        else {
+          log.debug("  -> ${role} already present");
+        }
+      }
     }
-    def destroy = {
-    }
+  }
 }
---
+++ config/MonitorSvcG2-config.groovy
@@ -0,0 +1,33 @@
+
+
+sysusers = [
+  [
+    name:'userb',
+    pass:'userb',
+    display:'UserB',
+    email:'read@localhost',
+    roles:['ROLE_USER','INST_USER']
+  ],
+    [
+    name:'userc',
+    pass:'userc',
+    display:'UserC',
+    email:'read@localhost',
+    roles:['ROLE_USER','INST_USER'
+]  ],
+    [
+    name:'usera',
+    pass:'usera',
+    display:'UserA',
+    email:'read@localhost',
+    roles:['ROLE_USER','INST_USER']
+  ],
+    [
+    name:'admin',
+    pass:'admin',
+    display:'TestAdmin',
+    email:'read@localhost',
+    roles:['ROLE_USER','ROLE_ADMIN',"INST_ADM"]
+  ]
+]
+
