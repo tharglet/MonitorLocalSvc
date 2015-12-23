3351f883245132ebf58db51e7903c1bf3f061fb3
Ian Ibbotson <ian.ibbotson@gmail.com>
2015-12-23 19:30:33 +0000

    Create self OAuth Client - separate out auth service from app which uses

--- MonitorSvcG2/grails-app/conf/BootStrap.groovy
+++ MonitorSvcG2/grails-app/conf/BootStrap.groovy
@@ -6,12 +6,23 @@ class BootStrap {
 
   def init = { servletContext ->
 
-    def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER', roleType:'global').save(failOnError: true)
-    def editorRole = Role.findByAuthority('ROLE_EDITOR') ?: new Role(authority: 'ROLE_EDITOR', roleType:'global').save(failOnError: true)
-    def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN', roleType:'global').save(failOnError: true)
-    def apiRole = Role.findByAuthority('ROLE_API') ?: new Role(authority: 'ROLE_API', roleType:'global').save(failOnError: true)
+    def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
+    def editorRole = Role.findByAuthority('ROLE_EDITOR') ?: new Role(authority: 'ROLE_EDITOR').save(failOnError: true)
+    def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+    def apiRole = Role.findByAuthority('ROLE_API') ?: new Role(authority: 'ROLE_API').save(failOnError: true)
+    def clientRole = Role.findByAuthority('ROLE_CLIENT') ?: new Role(authority: 'ROLE_CLIENT').save(failOnError: true)
 
     makeDefaultAccounts()
+
+    def monitor_client = OAuthClient.findByClientId('monitorLocal') ?: 
+         new OAuthClient(
+                   clientId: 'monitorLocal',
+                   clientSecret: 'a3802436c3c558ea5725e40756fa7b16',
+                   authorizedGrantTypes: ['authorization_code', 'refresh_token', 'implicit', 'password', 'client_credentials'],
+                   authorities: ['ROLE_CLIENT'],
+                   scopes: ['read','write'],
+                   redirectUris: ['http://localhost:8080/MonitorSvcG2/redirect']
+       ).save(flush: true)
   }
 
 
