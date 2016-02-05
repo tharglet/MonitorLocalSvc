d2bdc00161ab91eec116d0b13fd3941352f8e656
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-02-05 18:58:47 +0000

    Changed types of properties to identifiers.

--- MonitorLocalSvcG3/grails-app/domain/uk/ac/jisc/monitorlocal/Person.groovy
+++ MonitorLocalSvcG3/grails-app/domain/uk/ac/jisc/monitorlocal/Person.groovy
@@ -1,10 +1,12 @@
 package uk.ac.jisc.monitorlocal
 
+import com.k_int.grails.tools.identifiers.Identifier
+
 class Person extends Component {
   
+  Identifier orcid
+  Identifier reference
   String email
-  String orcid
-  String internalID
 
   static constraints = {
   }
