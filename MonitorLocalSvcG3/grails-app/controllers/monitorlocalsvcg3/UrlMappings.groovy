ecf49be4d4e3e44a1f31d10c5cbb577901891223
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-03-12 10:08:04 +0000

    Latest WIP after reverting to default build defs.

--- MonitorLocalSvcG3/grails-app/controllers/monitorlocalsvcg3/UrlMappings.groovy
+++ MonitorLocalSvcG3/grails-app/controllers/monitorlocalsvcg3/UrlMappings.groovy
@@ -3,11 +3,7 @@ package monitorlocalsvcg3
 class UrlMappings {
 
   static mappings = {
-
-    "/ao" (controller: 'academicOutput', action: 'query', method: 'QUERY')
-    "/ao/$q?" (controller: 'academicOutput', action: 'query', method: 'QUERY')
-    "/ao/query(.(*))?" (controller: 'academicOutput', action: 'query', method: 'GET')
-    "/ao/search(.(*))?" (controller: 'academicOutput', action: 'query', method: 'GET')
+    "/ao/query/${q}?" (controller: 'academicOutput', action: 'query', method: 'GET')
 
     "/jwt/${action}/${provider}"(controller: 'jwt')
 
