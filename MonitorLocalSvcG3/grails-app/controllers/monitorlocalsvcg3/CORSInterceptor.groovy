d95ac38b8280cdc7e233162ed6737fb0d1387086
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-03-14 10:15:12 +0000

    Working without ES for the time being.

--- MonitorLocalSvcG3/grails-app/controllers/monitorlocalsvcg3/CORSInterceptor.groovy
+++ MonitorLocalSvcG3/grails-app/controllers/monitorlocalsvcg3/CORSInterceptor.groovy
@@ -2,18 +2,18 @@ package monitorlocalsvcg3
 
 class CORSInterceptor {
   CORSInterceptor() {
-//    log.debug ("Initialising Interceptor")
-//    matchAll()
+    log.debug ("Initialising Interceptor")
+    matchAll()
   }
   
   boolean before() {
-//    header( "Access-Control-Allow-Origin", "http://localhost:9090" )
-//    header( "Access-Control-Allow-Credentials", "true" )
-//    header( "Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, QUERY" )
-//    header( "Access-Control-Allow-Headers", "X-Requested-With, Access, Content-Type" )
-//    
-//    log.debug ("Executing before interceptor")
-//    
+    header( "Access-Control-Allow-Origin", "*" )
+    header( "Access-Control-Allow-Credentials", "true" )
+    header( "Access-Control-Allow-Methods", "POST, GET, PUT, DELETE" )
+    header( "Access-Control-Allow-Headers", "x-requested-with,access,accept,content-type" )
+    
+    log.debug ("Executing before interceptor")
+    
     true
   }
 
