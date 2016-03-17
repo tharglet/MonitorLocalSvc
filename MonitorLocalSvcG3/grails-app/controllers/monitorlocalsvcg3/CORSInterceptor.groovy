9268ed4bbb7f7d4aa7219a1ec50484a009bb213b
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-03-17 18:48:42 +0000

    Magic needed to stop a 404 being returned by the server when browser runs it's "pre-flight checks" for updates.*sigh*

--- MonitorLocalSvcG3/grails-app/controllers/monitorlocalsvcg3/CORSInterceptor.groovy
+++ MonitorLocalSvcG3/grails-app/controllers/monitorlocalsvcg3/CORSInterceptor.groovy
@@ -1,19 +1,27 @@
 package monitorlocalsvcg3
 
 class CORSInterceptor {
+
+  int order = HIGHEST_PRECEDENCE;
+
   CORSInterceptor() {
-    log.debug ("Initialising Interceptor")
     matchAll()
   }
-  
+
   boolean before() {
-    header( "Access-Control-Allow-Origin", "*" )
-    header( "Access-Control-Allow-Credentials", "true" )
-    header( "Access-Control-Allow-Methods", "POST, GET, PUT, DELETE" )
-    header( "Access-Control-Allow-Headers", "x-requested-with,access,accept,content-type" )
+        
+    header("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS")
+    header("Access-Control-Allow-Origin", "*" )
+    header("Access-Control-Allow-Credentials", "true" )
+    header("Access-Control-Allow-Headers", "Origin, Authorization,X-Requested-With,Access,Accept,Content-Type" )
     
-    log.debug ("Executing before interceptor")
-    
+    // If this is an OPTIONS request type, then the browser is performing some pre-flight
+    // requests to the server to see if it is indeed allowed to send the data here.
+    if (request.method == "OPTIONS") {
+      render (status : 200)
+      return false
+    }
+
     true
   }
 
