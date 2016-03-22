5296489faa299925c2fec75b802d41659e95c025
Ian Ibbotson <ianibbo@gmail.com>
2016-03-22 12:29:48 +0000

    More work on KB+ sync title data

--- MonitorLocalSvcG3/grails-app/services/com/k_int/kbplus/KBPlusSyncService.groovy
+++ MonitorLocalSvcG3/grails-app/services/com/k_int/kbplus/KBPlusSyncService.groovy
@@ -6,6 +6,7 @@ import grails.converters.*
 import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.ContentType.*
 import static groovyx.net.http.Method.*
+import uk.ac.jisc.monitorlocal.*
 import grails.transaction.Transactional
 
 import groovyx.net.http.*
@@ -22,12 +23,16 @@ class KbplusSyncService {
 
 
   def getLatestKBPlusTitles() {
-    doSync('http://www.kbplus.ac.uk','/test/oai/titles', 'kbplus', 0) { r ->
+    def cursor = SyncCursor.findByActivity('KBPlusTitles') ?: new SyncCursor(activity:'KBPlusTitles').save(flush:true, failOnError:true);
+
+    doSync('http://www.kbplus.ac.uk','/test/oai/titles', 'kbplus', cursor) { r ->
+      def result = [:]
       log.debug("Process record ${r}");
+      result
     }
   }
 
-  public doSync(host, path, notificationTarget, prefix, ts) {
+  public doSync(host, path, notificationTarget, prefix, cursor) {
     println("Get latest changes");
 
     def http = new HTTPBuilder( host )
@@ -44,7 +49,11 @@ class KbplusSyncService {
           uri.query = [ verb:'ListRecords', resumptionToken: resumption ]
         }
         else {
-          uri.query = [ verb:'ListRecords', metadataPrefix: prefix ]
+          def the_date = new Date(cursor.highestTimestamp)
+          def sdf = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+          def from=sdf.format(the_date)
+          log.debug("Requesting records since ${cursor.highestTimestamp} :: ${from}");
+          uri.query = [ verb:'ListRecords', metadataPrefix: prefix, from:from ]
         }
 
         // response handler for a success response code:
@@ -52,9 +61,14 @@ class KbplusSyncService {
           println resp.statusLine
 
           xml.'ListRecords'.'record'.each { r ->
-            notificationTarget(r)
+            def clr = notificationTarget(r)
+            log.debug(clr);
           }
         }
+
+        // update cursor
+        cursor.lastTimestamp = highestTimestamp
+        cursor.save(flush:true, failOnError:true);
       }
     }
   }
