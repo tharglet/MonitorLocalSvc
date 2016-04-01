49d8d32ffb7f18196920b5c4b3ae9036385c509e
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-04-01 12:17:38 +0100

    Annotated the org so it becomes a RESTful resource. Added address taxcode and type to Org.

--- MonitorLocalSvcG3/grails-app/domain/uk/ac/jisc/monitorlocal/Org.groovy
+++ MonitorLocalSvcG3/grails-app/domain/uk/ac/jisc/monitorlocal/Org.groovy
@@ -1,4 +1,18 @@
 package uk.ac.jisc.monitorlocal
 
+import grails.rest.Resource
+
+import com.k_int.grails.tools.refdata.*
+import com.k_int.grails.tools.rest.ExtendedRestfulController
+
+@Resource(uri="/org", superClass=ExtendedRestfulController)
 class Org extends Component {
+  String address
+  String taxCode
+  
+  @Defaults([
+    'Organisation type 1',
+    'Organisation type 2'
+  ])
+  RefdataValue type
 }
