58c44f4d9f09c118ca94310c0b8142617c7b74ad
Ian Ibbotson <ianibbo@gmail.com>
2016-04-05 16:46:35 +0100

    Added a unit test that checks out using data binding against AO. initially just set title

---
+++ MonitorLocalSvcG3/src/test/groovy/com/k_int/monitor/AOBindingTest.groovy
@@ -0,0 +1,43 @@
+package com.k_int.monitor
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+import uk.ac.jisc.monitorlocal.*
+import grails.test.mixin.TestMixin
+import grails.test.mixin.support.GrailsUnitTestMixin
+import grails.databinding.SimpleMapDataBindingSource
+
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestMixin(GrailsUnitTestMixin)
+class AOBindingTest extends Specification {
+    
+    def dataBinder
+
+    def setup() {
+        // Use Grails data binding
+        dataBinder = applicationContext.getBean('grailsWebDataBinder')
+    }
+
+    def cleanup() {
+    }
+
+    void "Binding name to AO should set name property"() {
+        given:
+        final AcademicOutput ao = new AcademicOutput()
+
+        and:
+        final SimpleMapDataBindingSource source = 
+            [id: 1, name: 'My AO Name 1']
+
+        when:
+        dataBinder.bind(ao, source)
+
+        then:
+        with(ao) {
+            name == 'My AO Name 1'
+        }
+    }
+}
