81c9a1bace6d198625907fa29ab059d9652112c2
Ian Ibbotson <ianibbo@gmail.com>
2016-04-07 14:39:38 +0100

    started work on merge identifiers

--- MonitorLocalSvcG3/grails-app/domain/uk/ac/jisc/monitorlocal/ComponentIdentifier.groovy
+++ MonitorLocalSvcG3/grails-app/domain/uk/ac/jisc/monitorlocal/ComponentIdentifier.groovy
@@ -2,15 +2,36 @@ package uk.ac.jisc.monitorlocal
 
 import groovy.transform.EqualsAndHashCode
 import com.k_int.grails.tools.identifiers.Identifier
+import org.grails.databinding.BindUsing
+import grails.databinding.SimpleMapDataBindingSource
+import javax.persistence.Transient
+import groovy.util.logging.Log4j
 
+@Log4j
 class ComponentIdentifier {
 
+  @BindUsing({obj,source ->
+    log.debug("Bind identifier in componentIdentifier ${obj} ${source}");
+    obj['identifier']
+  })
   Identifier identifier
   Component component
 
   static constraints = {
     identifier nullable: false, blank:false
     component nullable: false, blank:false
+  }
+
+  static ComponentIdentifier fuzzyMatch(ci) {
+
+    def result = null;
+
+    if ( ci['id'] ) {
+      println("lookup component identifier by id ${ci['id']}");
+      result = ComponentIdentifier.get(ci['id']);
+    }
+
+    result;
   }
 
 }
