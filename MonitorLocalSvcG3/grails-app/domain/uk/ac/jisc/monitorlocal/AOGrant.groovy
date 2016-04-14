ad202b47db408012e70b7bb30c5ba0a3ea7856df
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-04-14 13:41:54 +0100

    Grant tidy.

--- MonitorLocalSvcG3/grails-app/domain/uk/ac/jisc/monitorlocal/AOGrant.groovy
+++ MonitorLocalSvcG3/grails-app/domain/uk/ac/jisc/monitorlocal/AOGrant.groovy
@@ -1,12 +1,9 @@
 package uk.ac.jisc.monitorlocal
 
-import com.k_int.grails.tools.identifiers.Identifier
 
 /**
- *  A name attached to an AO - the link between AO and person, with the ability to specify a name.
- *  We may have a name, but no way to uniquely look up or resolve that name into an identified person.
- *  In that case, the relationship from an AO may be terminated by here and not link to a Person, allowing
- *  an admin to resolve the ambiguity later, whilst still recording the name.
+ *  A Grant attached to an AO - May contain between one and all of:
+ *  A funder, the fund name and the grant ID 
  */
 class AOGrant {
   
