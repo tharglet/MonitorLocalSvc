37235fa548b36f036460867607b8c2e2df49deed
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-04-16 14:21:40 +0100

    Default Thread uncaught exception listener. Why did I not know about this before.

--- MonitorLocalSvcG3/grails-app/init/monitorlocalsvcg3/Application.groovy
+++ MonitorLocalSvcG3/grails-app/init/monitorlocalsvcg3/Application.groovy
@@ -2,9 +2,18 @@ package monitorlocalsvcg3
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.apache.commons.lang3.exception.ExceptionUtils
 
 class Application extends GrailsAutoConfiguration {
   static void main(String[] args) {
+    
+    Thread.setDefaultUncaughtExceptionHandler(
+      new Thread.UncaughtExceptionHandler() {
+        @Override public void uncaughtException(Thread t, Throwable e) {
+          System.out.println(ExceptionUtils.getStackTrace(e));
+        }
+      }
+    );
     GrailsApp.run(Application, args)
   }
 }
