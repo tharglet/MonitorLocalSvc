7f148565c242dcd9f0d430f5e8facc3448f83124
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-04-18 09:25:49 +0100

    Apache commons 3 was present in IDE but not on classpath. Reusing version 2 import that comes with Grails instead.

--- MonitorLocalSvcG3/grails-app/init/monitorlocalsvcg3/Application.groovy
+++ MonitorLocalSvcG3/grails-app/init/monitorlocalsvcg3/Application.groovy
@@ -2,7 +2,8 @@ package monitorlocalsvcg3
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
-import org.apache.commons.lang3.exception.ExceptionUtils
+
+import org.apache.commons.lang.exception.ExceptionUtils
 
 class Application extends GrailsAutoConfiguration {
   static void main(String[] args) {
@@ -10,7 +11,7 @@ class Application extends GrailsAutoConfiguration {
     Thread.setDefaultUncaughtExceptionHandler(
       new Thread.UncaughtExceptionHandler() {
         @Override public void uncaughtException(Thread t, Throwable e) {
-          System.out.println(ExceptionUtils.getStackTrace(e));
+          System.out.println(ExceptionUtils.getFullStackTrace(e));
         }
       }
     );
