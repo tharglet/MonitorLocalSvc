b8db7c474f8416864af2c7f4e8b8edb13e31be8e
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-04-19 08:42:17 +0100

    Reinstate loop for all files.

--- testing/apc_sheets/upload.sh
+++ testing/apc_sheets/upload.sh
@@ -2,18 +2,18 @@
 
 # for file in `ls *APC*.csv`
 
-#curl -v --user admin:admin -X POST --form content=@./s.csv --form instname="University of Sheffield" http://localhost:8080/application/apcIngest
+# curl -v --user admin:admin -X POST --form content=@./s.csv --form instname="University of Sheffield" http://localhost:8080/application/apcIngest
 
- files="*.csv"
- regex="([^\-]+)-APC.+\.csv"
- for f in $files
- do
-     if [[ $f =~ $regex ]]
-     then
-         institution="${BASH_REMATCH[1]}"
-         echo "Sending file '${f}' for '${institution}'"
-         curl -v --user admin:admin -X POST --form content=@$f --form instname="${institution}" http://localhost:8080/application/apcIngest
-     else
-         echo "Skipping '$f'"
-     fi
- done
+files="*.csv"
+regex="([^\-]+)-APC.+\.csv"
+for f in $files
+do
+    if [[ $f =~ $regex ]]
+    then
+        institution="${BASH_REMATCH[1]}"
+        echo "Sending file '${f}' for '${institution}'"
+        curl -v --user admin:admin -X POST --form content=@$f --form instname="${institution}" http://localhost:8080/application/apcIngest
+    else
+        echo "Skipping '$f'"
+    fi
+done
