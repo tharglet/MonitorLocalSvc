3018b17e31e897d431098fc6fb68de1ca2937350
Ian Ibbotson <ianibbo@gmail.com>
2016-06-27 13:46:10 +0100

    Moved internal api functions to InternalApi controller, left application/settings in application controller. Added bootstrap code to look in user home DIR for  system level accounts - to allow upload processes to connect and trigger system level actions

---
+++ MonitorLocalSvcG3/grails-app/controllers/uk/ac/jisc/monitorlocal/InternalApi.groovy
@@ -0,0 +1,103 @@
+package uk.ac.jisc.monitorlocal
+
+import grails.converters.*
+import grails.core.GrailsApplication
+import grails.plugins.*
+import grails.plugin.springsecurity.annotation.Secured
+
+
+import com.k_int.grails.tools.finance.YahooRatesService
+
+
+class InternalApiController implements PluginManagerAware {
+  static responseFormats = ['json', 'xml']
+
+  GrailsApplication grailsApplication
+  GrailsPluginManager pluginManager
+  YahooRatesService yahooRatesService
+
+  def kbplusSyncService
+  def crossrefSyncService
+  def apcSheetImportService
+  def springSecurityService
+
+  def index() {
+    [grailsApplication: grailsApplication, pluginManager: pluginManager]
+  }
+
+  /**
+   *  /application/command/kbplus -- Sync with kb+
+   *  /application/command/AOWizard?doi=DOITOLOOKUP -- Create a new AO based on the DOI
+   */
+  def command() {
+    def response = [:]
+    log.debug("Command ${params}");
+    switch(params.id) {
+      case 'kbplus':
+        log.debug("Trigger KB+ Sync");
+        kbplusSyncService.getLatestKBPlusTitles()
+        break;
+      case 'AOWizard':
+        log.debug("AO Wizardi -- example :: curl -v -d \"doi=10.7567/ssdm.1986.c-3-1\" http://localhost:8080/application/command/AOWizard");
+        if ( params.doi ) {
+          crossrefSyncService.crossrefWizzard(params.doi);
+        }
+        break;
+
+      case 'exchange-rates':
+        log.debug("Grab the exchange rates from Yahoo");
+
+      // Fetched all rates
+        response = yahooRatesService.getAllRates()
+        break;
+      default:
+        log.debug("Unhandled command ${params.id}");
+        break;
+    }
+
+    render response as JSON
+  }
+
+
+  /**
+   *  curl -v --user admin:admin -X POST \ --form content=@./YBP1.tsv http://localhost:8080/application/apcIngest
+   *
+   */
+  def ApcIngest() {
+
+    def result = [:]
+    result.messages = []
+
+    log.debug("ApcIngest ${params}");
+
+    def upload_mime_type = request.getFile("content")?.contentType  // getPart?
+    def upload_filename = request.getFile("content")?.getOriginalFilename()
+    def new_datafile_id = null
+    def org = Org.findByName(params.instname) ?: new Org(name:params.instname)
+
+    // Set the type to HEI.
+    org.setTypeFromString('HEI')
+    org.save(flush:true, failOnError:true)
+
+    if ( upload_mime_type && upload_filename && org ) {
+
+      log.debug("got mime type, upload org is ${org}");
+
+      // def deposit_token = java.util.UUID.randomUUID().toString();
+      def upload_file = request.getFile("content");
+
+      log.debug("Calling ingest");
+      apcSheetImportService.assimilateApcSpreadsheet(org, upload_file.getInputStream(),upload_filename )
+
+    }
+    else {
+      if ( org == null ){
+        result.messages.add("Unable to locate org with name ${params.instname}. Please add and retry");
+      }
+    }
+
+    render result as JSON
+
+  }
+
+}
--- MonitorLocalSvcG3/grails-app/init/BootStrap.groovy
+++ MonitorLocalSvcG3/grails-app/init/BootStrap.groovy
@@ -13,8 +13,10 @@ class BootStrap {
     SpringSecurityUtils.clientRegisterFilter('jwtPreauthFilter', SecurityFilterPosition.PRE_AUTH_FILTER)
 
     log.debug("grailsApplication.config.monitorLocal_jwk : ${grailsApplication.config.monitorLocal_jwk}")
+
     def role_user = Role.findByAuthority('ROLE_USER') ?: new Role(authority:'ROLE_USER').save(flush:true, failOnError:true)
     def role_verified_user = Role.findByAuthority('ROLE_VERIFIED_USER') ?: new Role(authority:'ROLE_VERIFIED_USER').save(flush:true, failOnError:true)
+    def role_system = Role.findByAuthority('ROLE_SYSTEM') ?: new Role(authority:'ROLE_SYSTEM').save(flush:true, failOnError:true)
     
     // Run the rules.
 //    AcademicOutput ao = AcademicOutput.list(max:1)?.get(0)
@@ -23,10 +25,66 @@ class BootStrap {
 //    log.debug "${result}"
 //    System.out.println("${result}")
 
+    log.debug("Register User");
+    registerUsers();
+
 
   }
 
   def destroy = {
+  }
+
+  def registerUsers() {
+
+
+    def cfg_file = new File("${System.properties['user.home']}/.grails/monitor_users.groovy")
+    if ( cfg_file.exists() ) {
+      def cfg = cfg_file.text
+
+      def usercfg = new groovy.util.ConfigSlurper().parse(cfg);
+      log.debug("Processing additional users:: ${cfg} \n${usercfg} ");
+  
+      usercfg.sysusers.each { su ->
+        log.debug("test ${su.name} ${su.pass} ${su.display} ${su.roles}");
+        def user = User.findByUsername(su.name)
+        if ( user ) {
+          if ( user.password != su.pass ) {
+            log.debug("Hard change of user password from config ${user.password} -> ${su.pass}");
+            user.password = su.pass;
+            user.save(failOnError: true)
+          }
+          else {
+            log.debug("${su.name} present and correct");
+          }
+        }
+        else {
+          log.debug("Create user...");
+          user = new User(
+                          username: su.name,
+                          password: su.pass,
+                          name: su.display,
+                          email: su.email,
+                          enabled: true).save(failOnError: true)
+        }
+  
+        log.debug("Add roles for ${su.name}");
+        su.roles.each { r ->
+          def role = Role.findByAuthority(r)
+          if ( ! ( user.authorities.contains(role) ) ) {
+            log.debug("  -> adding role ${role}");
+            UserRole.create user, role
+          }
+          else {
+            log.debug("  -> ${role} already present");
+          }
+        }
+      }
+    }
+    else {
+      log.warn("No local user config file:: ${System.properties['user.home']}/.grails/monitor_users.groovy");
+    }
+
+
   }
 
 }
