9c1944ef71a636a17683942459414486fa067fbf
Ian Ibbotson <ian.ibbotson@k-int.com>
2016-07-13 14:18:01 +0100

    Made Budget institution specific

--- MonitorLocalSvcG3/grails-app/domain/uk/ac/jisc/monitorlocal/Budget.groovy
+++ MonitorLocalSvcG3/grails-app/domain/uk/ac/jisc/monitorlocal/Budget.groovy
@@ -86,4 +86,44 @@ class Budget extends Component {
     totalFunds cascade: "all-delete-orphan"
     allocatedCosts cascade: "all"
   }
+
+  public static Map getSearchConfig() {
+    log.debug("getSearchConfig()");
+
+    return [
+      baseclass:'uk.ac.jisc.monitorlocal.Budget',
+      title:'Budget',
+      group:'Secondary',
+      defaultSort:'name',
+      defaultOrder:'asc',
+      qbeConfig:[
+        qbeForm:[
+          [
+            prompt:'Name or Title',
+            qparam:'q',
+            placeholder:'Name or title of item',
+            contextTree: [ 'ctxtp':'disjunctive',
+                             'terms':[
+                                  ['ctxtp':'qry', 'comparator' : 'ilike', 'prop':'name', 'wildcard':'R']
+                             ]
+                         ]
+
+          ],
+          [
+            prompt:'Owner Institution',
+            qparam:'instCtx',
+            placeholder:'Owner Institution',
+            contextTree: [ 'ctxtp':'qry', 'comparator' : 'eq', 'prop':'ownerInstitution.id', type:'java.lang.Long' ],
+          ]
+        ],
+        qbeGlobals:[
+        ],
+        qbeResults:[
+          [heading:'Name', property:'name',sort:'name', link:[controller:'resource',action:'show',id:'x.r.class.name+\':\'+x.r.id'] ],
+          [heading:'Status', sort:'status', property:'status.value'],
+        ]
+      ]
+    ]
+  }
+
 }
