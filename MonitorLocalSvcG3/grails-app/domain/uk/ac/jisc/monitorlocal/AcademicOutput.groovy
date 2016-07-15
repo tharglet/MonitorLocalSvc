b784610965cc07ac8d529db4c03d2f1042b75fb0
Ian Ibbotson <ian.ibbotson@k-int.com>
2016-07-15 08:43:29 +0100

    OA searches leading wildcard

--- MonitorLocalSvcG3/grails-app/domain/uk/ac/jisc/monitorlocal/AcademicOutput.groovy
+++ MonitorLocalSvcG3/grails-app/domain/uk/ac/jisc/monitorlocal/AcademicOutput.groovy
@@ -238,9 +238,9 @@ class AcademicOutput extends Component {
             placeholder:'Name or title of item',
             contextTree: [ 'ctxtp':'disjunctive',
                              'terms':[
-                                  ['ctxtp':'qry', 'comparator' : 'ilike', 'prop':'name', 'wildcard':'R'],
-                                  ['ctxtp':'qry', 'comparator' : 'ilike', 'prop':'identifiers.identifier.value', 'wildcard':'R'],
-                                  ['ctxtp':'qry', 'comparator' : 'ilike', 'prop':'assignedTo.name', 'wildcard':'R']
+                                  ['ctxtp':'qry', 'comparator' : 'ilike', 'prop':'name', 'wildcard':'B'],
+                                  ['ctxtp':'qry', 'comparator' : 'ilike', 'prop':'identifiers.identifier.value', 'wildcard':'B'],
+                                  ['ctxtp':'qry', 'comparator' : 'ilike', 'prop':'assignedTo.name', 'wildcard':'B']
                              ]
                          ]
 
