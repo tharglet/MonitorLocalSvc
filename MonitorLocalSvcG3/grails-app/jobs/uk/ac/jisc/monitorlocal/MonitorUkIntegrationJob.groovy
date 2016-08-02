53a8ff341b206d9024260d5d0eb4dd6f8caf2e66
Ian Ibbotson <ian.ibbotson@gmail.com>
2016-08-02 17:13:51 +0100

    MOre debug in monitor UK push

--- MonitorLocalSvcG3/grails-app/jobs/uk/ac/jisc/monitorlocal/MonitorUkIntegrationJob.groovy
+++ MonitorLocalSvcG3/grails-app/jobs/uk/ac/jisc/monitorlocal/MonitorUkIntegrationJob.groovy
@@ -5,10 +5,11 @@ class MonitorUkIntegrationJob {
     static boolean running = false;
 
     static triggers = {
-      simple repeatInterval: 600000l // execute job once in 600 seconds == every 10 mins
+      simple repeatInterval: 60000l // execute job once in 600 seconds == every 60 seconds
     }
 
     def synchronized execute() {
+      log.debug("MonitorUkIntegrationJob::execute()");
       if ( !running ) {
         running = true;
         pushToMonitorUK();
@@ -17,13 +18,20 @@ class MonitorUkIntegrationJob {
 
     def pushToMonitorUK() {
       // execute job
+      log.debug("MonitorUkIntegrationJob::pushToMonitorUK");
       try {
         if ( grailsApplication.config.pushToMonitorUK ) {
           def bc = BatchCursor.findByDomainClassNameAndActivity(AcademicOutput.class.name,'UKFeed') ?: new BatchCursor(domainClassName:AcademicOutput.class.name,activity:'UKFeed',lastTimestamp:0).save(flush:true, failOnError:true);
           def max_ts = bc.lastTimestamp;
-          def aos = AcademicOutput.executeQuery('select ao from AcademicOutput as ao where ao.ownerInstitution.monitorLocalAPIKey is not null AND ao.lastUpdated > :ts',[ts:ts]);
+ 
+          log.debug("Fetch all AOs for orgs with a monitor UK account modified after ${max_ts}");
+
+          def aos = AcademicOutput.executeQuery('select ao from AcademicOutput as ao where ao.ownerInstitution.monitorLocalAPIKey is not null AND ao.lastUpdated > :ts',[ts:max_ts]);
           aos.each { ao ->
             log.debug("Check that AO has the necessary fields (DOI) and dispatch using api key ${ao.ownerInstitution.monitorLocalAPIKey}");
+
+            // https://github.com/JiscMonitor/monitor-uk/blob/develop/docs/API/CONTRIBUTION.md
+
             def last_updated_for_this_ao = ao.lastUpdated?.getTime() ?: 0
             if ( last_updated_for_this_ao > max_ts ) {
               max_ts = last_updated_for_this_ao;
