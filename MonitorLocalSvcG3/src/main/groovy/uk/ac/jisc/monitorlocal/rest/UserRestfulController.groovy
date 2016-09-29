14301d280bf8943db094ca810a32e382d3e6e3e0
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-09-29 20:33:16 +0100

    Still failing once compiled.

--- MonitorLocalSvcG3/src/main/groovy/uk/ac/jisc/monitorlocal/rest/UserRestfulController.groovy
+++ MonitorLocalSvcG3/src/main/groovy/uk/ac/jisc/monitorlocal/rest/UserRestfulController.groovy
@@ -1,17 +1,20 @@
 package uk.ac.jisc.monitorlocal.rest;
 
 import grails.artefact.Artefact
-import grails.plugin.springsecurity.SpringSecurityService
-import grails.plugins.GrailsPluginManager;
+import grails.plugins.GrailsPluginManager
 
-import javax.persistence.Transient;
+import javax.persistence.Transient
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.access.prepost.PreAuthorize
 
 import com.k_int.grails.tools.rest.ExtendedRestfulController
 @Artefact("Controller")
 public class UserRestfulController<User> extends ExtendedRestfulController<User> {
   
+  @Transient
+  @Autowired(required = true)
+  def springSecurityService
   
   UserRestfulController (Class<User> domainClass) {
       this(domainClass, false)
