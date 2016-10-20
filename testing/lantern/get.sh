4814bb60f3cdde70b9236afacf7e6066f82b02ff
Ian Ibbotson <ianibbo@gmail.com>
2016-10-20 12:17:19 +0100

    Lantern integration scripts for demo

---
+++ testing/lantern/get.sh
@@ -0,0 +1,2 @@
+curl -XGET -i "https://api.cottagelabs.com/service/lantern/NEaSYwQpwaGZphDrE?apikey=XXX"
+curl -XGET -i "https://api.cottagelabs.com/service/lantern/QfPaQvmc8xpXqEq6f/results?apikey=XXX"
